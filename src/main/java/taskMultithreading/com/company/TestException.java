9c2829e0ae810bffc87c2422d9902c8b360b56b3
kdobr <kdobr77@gmail.com>
2019-12-04 14:26:32 +0300

    add Multithreading

---
+++ src/main/java/taskMultithreading/com/company/TestException.java
@@ -0,0 +1,14 @@
+package taskMultithreading.com.company;
+
+/**
+ * Не менять.
+ */
+public class TestException extends Exception {
+    public TestException(String message) {
+        super(message);
+    }
+
+    TestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
